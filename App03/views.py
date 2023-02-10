6424a79d46aae971526aed0b832f7bf768eba1b5
Gianfranco Valente Cano <gianfrancovalentecano@gmail.com>
2023-02-10 17:41:05 -0300

    Checkpoint. templates funcionando y funciona la barra

--- App03/views.py
+++ App03/views.py
@@ -3,13 +3,6 @@ from django.http import HttpResponse
 from App03.models import Curso
 
 # Create your views here.
-'''
-def curso(self):
-    curso = Curso(nombre = 'Desarrollo web', comision = '19881') #igual que el nombre de la clase en models
-    curso.save()
-    documentoTexto = f'--> Curso: {curso.nombre} comision: {curso.comision}'
-    return HttpResponse(documentoTexto)
-'''
 
 def inicio(request):
     return render(request, 'App03/inicio.html') # usar el nombre del template
